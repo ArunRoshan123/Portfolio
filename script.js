afe816bbb9f186b72120f692bd525f51ee6d7979
ArunRoshan123 <arunraksha234@gmail.com>
2023-03-18 00:05:31 +0530

    Save

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        // scroll-up button show/hide script
         if(this.scroll > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
-
         }
     });
 
     // slide up script 
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
@@ -26,18 +34,38 @@ $(document).ready(function(){
 
     // typing animation script 
     var typed = new Typed(".typing", {
-        strings:["Developer.","Blogger.","Designer.","Coder.","Freelancer."],
+        strings:["Student.","Developer.","Blogger.","Web Designer.","Freelancer."],
         typeSpeed: 100,
         backspeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings:["Developer.","Blogger.","Designer.","Coder.","Freelancer."],
+        strings:["Student.","Developer.","Blogger.","Web Designer.","Freelancer."],
         typeSpeed: 100,
         backspeed: 60,
         loop: true
     });
 
-
-
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
